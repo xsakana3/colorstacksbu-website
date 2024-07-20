0133145918f7ac9a593e197a17ca0d12621fcec2
jbon <v-joshboone@microsoft.com>
2024-07-20 13:21:02 -0700

    initial setup, including listing of colors used by main site in App.css and setup of AppRouter.jsx for temporary routing of currently empty pages (will possibly be replaced by component based routing in the future); <Link> tages have yet to be added

--- colorstacksbu/src/main.jsx
+++ colorstacksbu/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import AppRouter from './router/AppRouter.jsx'
 import './index.css'
+import { Router } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AppRouter />
+    </Router>
   </React.StrictMode>,
 )
